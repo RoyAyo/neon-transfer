ff0739e55b4922e67b2be547e7bb82faf2835f48
Roy Ayo <roylayindeay0@gmail.com>
2024-09-10 14:34:42 +0100

    switched files to main

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,30 +6,30 @@ import { IDEX, ITokens, TOKENS } from "../core/interfaces";
 require('dotenv').config({});
 
 export const NEON_PRIVATE = process.env.NEON_PRIVATE;
-export const PROXY_URL = `https://devnet.neonevm.org`;
-// export const PROXY_URL = `https://neon-proxy-mainnet.solana.p2p.org`;
+// export const PROXY_URL = `https://devnet.neonevm.org`;
+export const PROXY_URL = `https://neon-proxy-mainnet.solana.p2p.org`;
 
-export const WRAPPED_NEON_TOKEN: ITokens = {
-  name: TOKENS.WNEON,
-  address: "0x11adC2d986E334137b9ad0a0F290771F31e9517F",
-  decimal: NEON_TOKEN_MINT_DECIMALS
-}
 // export const WRAPPED_NEON_TOKEN: ITokens = {
 //   name: TOKENS.WNEON,
-//   address: "0x202C35e517Fa803B537565c40F0a6965D7204609",
+//   address: "0x11adC2d986E334137b9ad0a0F290771F31e9517F",
 //   decimal: NEON_TOKEN_MINT_DECIMALS
 // }
+export const WRAPPED_NEON_TOKEN: ITokens = {
+  name: TOKENS.WNEON,
+  address: "0x202C35e517Fa803B537565c40F0a6965D7204609",
+  decimal: NEON_TOKEN_MINT_DECIMALS
+}
 
-export const USDT_TOKEN: ITokens = {
-  name: TOKENS.USDT,
-  address: "0x6eEf939FC6e2B3F440dCbB72Ea81Cd63B5a519A5",
-  decimal: 6
-};
 // export const USDT_TOKEN: ITokens = {
 //   name: TOKENS.USDT,
-//   address: "0x5f0155d08eF4aaE2B500AefB64A3419dA8bB611a",
+//   address: "0x6eEf939FC6e2B3F440dCbB72Ea81Cd63B5a519A5",
 //   decimal: 6
 // };
+export const USDT_TOKEN: ITokens = {
+  name: TOKENS.USDT,
+  address: "0x5f0155d08eF4aaE2B500AefB64A3419dA8bB611a",
+  decimal: 6
+};
 
 export const FIXED_TOKENS_TO_APPROVE = "1000";
 export const slippage = 98; // %;
@@ -53,7 +53,7 @@ export const ERC20_ABI = [
 export const DEXS: IDEX[] = [
   {
       name: "Moraswap",
-      router: "0x491FFC6eE42FEfB4Edab9BA7D5F3e639959E081B",
+      router: "0x594e37b9f39f5d31dec4a8c1cc4fe2e254153034",
       abi: [
         "function getAmountsOut(uint256 amountIn, address[] memory path) public view returns (uint[] memory amounts)",
         "function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] calldata path, address to, uint256 deadline)"
